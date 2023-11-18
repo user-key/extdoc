0b7b5a24754620053e5e1d095287737389cae0b6
Yuuzaki Masahiro <user-key@joy.email.ne.jp>
2023-11-18 21:17:42 +0900

    first commit

---
+++ config.js
@@ -0,0 +1,14 @@
+window.MathJax = {
+tex: {
+tags: "ams",
+macros: {
+x: "{\\times}",
+bm: ["{\\boldsymbol{#1}}",1],
+dd: ["{\\frac{\\partial #1}{\\partial #2}}",2]
+}
+},
+chtml: {
+scale: 0.9,
+mtextInheritFont: true
+}
+};
